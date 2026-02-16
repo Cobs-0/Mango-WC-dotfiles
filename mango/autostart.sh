97df90cb114e07a2bce6e1b69f3db08b90f16a3d
Cobs-0 <cianob7@gmail.com>
2026-02-16 22:30:11 +0000

    wtf is goin on

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -3,16 +3,9 @@
 # Kill old instances
 killall -q swaybg
 
-# Launch foot server
-foot --server &
-
 # Wait for colors to be ready (from startup-wal)
 sleep 0.5
 
 # Launch Waybar (now that colors are set)
 waybar &
 dunst &
-
-# Cursor
-export XCURSOR_THEME=Adwaita
-export XCURSOR_SIZE=48
